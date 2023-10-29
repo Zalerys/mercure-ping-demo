33b637654c4b16271b44c4a39f77a17232aaa24b
M3dzZ <mehdichaibi05@gmail.com>
2023-10-29 16:57:47 +0100

    feat: add add in private message

--- frontend/src/Component/User.js
+++ frontend/src/Component/User.js
@@ -1,25 +1,22 @@
 import React from "react";
 import { UserCircle2 } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function User({ user, handleClick }) {
-
   return (
     <div key={user.id}>
-        <button
-          onClick={() => handleClick(user.id)}
-          className="bg-white border-2 rounded-md border-sky-800 w-full mb-2 items-center p-1 flex flex-row gap-2 hover:bg-sky-200"
-          type="submit"
-          value={user.id}
-        >
-          <div className="">
-            <UserCircle2
-              color="#075985"
-              size={28}
-            />
-          </div>
-          {user.username}
-        </button>
-      </div>
+      <button
+        onClick={() => handleClick(user.id)}
+        className="flex flex-row items-center w-full gap-2 p-3 text-black text hover:bg-blue-200"
+        type="submit"
+        value={user.id}
+      >
+        <div className="">
+          <UserCircle2 className="" color="#000000" size={28} />
+        </div>
+        {user.username}
+      </button>
+    </div>
   );
 }
 
